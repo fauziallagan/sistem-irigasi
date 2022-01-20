05d813cf5ece208abe3e25980ee4c520ff794bf4
fauziallagan <fauziallagan@gmail.com>
2022-01-20 16:07:41 +0700

    update register

--- register.php
+++ register.php
@@ -2,31 +2,6 @@
 session_start();
 require_once "connection.php";
 
-if (empty($_SESSION["error"])) {
-	$s_error = "";
-} else {
-	$s_error = $_SESSION["error"];
-	$_SESSION["error"] = "";
-}
-if (empty($_SESSION["warning"])) {
-	$s_warning = "";
-} else {
-	$s_warning = $_SESSION["warning"];
-	$_SESSION["warning"] = "";
-}
-if (empty($_SESSION["info"])) {
-	$s_info = "";
-} else {
-	$s_info = $_SESSION["info"];
-	$_SESSION["info"] = "";
-}
-if (empty($_SESSION["success"])) {
-	$s_success = "";
-} else {
-	$s_success = $_SESSION["success"];
-	$_SESSION["success"] = "";
-}
-
 $e_tot = 0;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -94,11 +69,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		// prepare sql and bind parameters
-		$stmt = $conn->prepare("INSERT INTO pengguna (kategori, username, paswd, email, nama)
-		VALUES (:kategori, :username, :paswd, :email, :nama)");
+		$stmt = $conn->prepare("INSERT INTO pengguna (kategori, username, pass, email, nama)
+		VALUES (:kategori, :username, :pass, :email, :nama)");
 		$stmt->bindValue(':kategori', $kategori);
 		$stmt->bindValue(':username', $username);
-		$stmt->bindValue(':paswd', $paswd);
+		$stmt->bindValue(':pass', $paswd);
 		$stmt->bindValue(':email', $email);
 		$stmt->bindValue(':nama', $nama);
 		$stmt->execute();
