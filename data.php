b0d2147a6da01fad5e5373f7e9fff54a060e362f
fauziallagan <fauziallagan@gmail.com>
2022-01-26 14:54:11 +0700

    add data

--- data.php
+++ data.php
@@ -1,6 +1,12 @@
 <?php 
 
-$d1 = 28;
+$d1 = 25;
 $d2 = 24;
 $d3 = 12.5;
+
+$testing_id_mesin = "PDM001";
+$testing_pin = "A1";
+$testing_nilai = 200;
+$testing_tegangan_max = 255;
+$calibration_content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum autem distinctio libero atque ratione sit, facilis saepe quibusdam magnam alias?"
 ?>
