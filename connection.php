c882591fc48b5a07917e148b74da4991fd6d9abe
fauziallagan <fauziallagan@gmail.com>
2022-02-02 20:07:06 +0700

    update connection database

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $usernamedb = "pdam";
 $passworddb = "Aez*27km!zz";
 $dbname = "pdm"; 
-
+$pdo = "mysql:host=$servername; dbname=$dbname";
 function saring($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -11,4 +11,6 @@ function saring($data) {
   return $data;
 }
 
+$connection = new PDO($pdo, $usernamedb, $passworddb);
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
