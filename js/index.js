80cadd5a3994edfb09d7a1e302fe65959629b163
fauziallagan <fauziallagan@gmail.com>
2022-02-17 16:55:16 +0700

    add limit time

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-
 window.setTimeout(function() {
     $(".alert-notif").fadeTo(500, 0).slideUp(500, function(){
         $(this).remove(); 
     });
-}, 2000);
+}, 3000);
 
 window.setTimeout(function(){
     $(".alert-redirect").fadeTo(500,0).slideUp(500, function(){
         window.location.replace("login.php");
     });
-}, 2000)
+}, 2000);
