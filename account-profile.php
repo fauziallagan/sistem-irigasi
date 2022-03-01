c0d0177874d72b9d7f0cafd6778fa0f0da38800e
fauziallagan <fauziallagan@gmail.com>
2022-03-01 15:12:44 +0700

    update profile

--- account-profile.php
+++ account-profile.php
@@ -1,5 +1,6 @@
 <?php 
 require "session.php";
+
 ?>
 <?php include "header.php"?>
         <div id="layoutSidenav">
@@ -126,7 +127,7 @@ require "session.php";
                                         <!-- Profile picture help block-->
                                         <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
                                         <!-- Profile picture upload button-->
-                                        <button class="btn btn-primary" type="button">Upload new image</button>
+                                        <button class="btn btn-primary disabled" type="button">Upload new image</button>
                                     </div>
                                 </div>
                             </div>
@@ -135,58 +136,35 @@ require "session.php";
                                 <div class="card mb-4">
                                     <div class="card-header">Account Details</div>
                                     <div class="card-body">
-                                        <form>
+                                        <form action="coba.php" method="post">
+                                            <!-- Form Group (name)-->
+                                            <div class="mb-3">
+                                                <label class="small mb-1" for="inputName">Name</label>
+                                                <input class="form-control" id="inputUsername" type="text" placeholder="Enter your Name" value="<?php echo $nama ?>" required autocomplete="false" name="nama" readonly = "readonly"/>
+                                            </div>
                                             <!-- Form Group (username)-->
                                             <div class="mb-3">
-                                                <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                                                <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username" />
+                                                <label class="small mb-1" for="inputUsername">Username </label>
+                                                <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="<?php echo $username ?>" required autocomplete="false" readonly = "readonly"/>
                                             </div>
+                                            <!-- Form Group (Email)-->
+                                           <div class="mb-3">
+                                                <label class="small mb-1" for="inputEmail">Email</label>
+                                                <input class="form-control" id="email" type="text" placeholder="Enter your Email" value="<?php echo $email?>" required autocomplete="false"  readonly = "readonly"/>
+                                            </div>  
                                             <!-- Form Row-->
-                                            <div class="row gx-3 mb-3">
-                                                <!-- Form Group (first name)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputFirstName">First name</label>
-                                                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie" />
-                                                </div>
-                                                <!-- Form Group (last name)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputLastName">Last name</label>
-                                                    <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna" />
-                                                </div>
-                                            </div>
-                                            <!-- Form Row        -->
-                                            <div class="row gx-3 mb-3">
-                                                <!-- Form Group (organization name)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputOrgName">Organization name</label>
-                                                    <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap" />
-                                                </div>
-                                                <!-- Form Group (location)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputLocation">Location</label>
-                                                    <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA" />
-                                                </div>
-                                            </div>
-                                            <!-- Form Group (email address)-->
                                             <div class="mb-3">
-                                                <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                                                <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="name@example.com" />
-                                            </div>
-                                            <!-- Form Row-->
-                                            <div class="row gx-3 mb-3">
-                                                <!-- Form Group (phone number)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputPhone">Phone number</label>
-                                                    <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567" />
-                                                </div>
-                                                <!-- Form Group (birthday)-->
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputBirthday">Birthday</label>
-                                                    <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988" />
-                                                </div>
+                                                <label class="small mb-1">Role</label>
+                                                <select class="form-select" aria-label="Default select example" disabled= "true">
+                                                    <option selected disabled>Select a role:</option>
+                                                    <option value="administrator" selected>Administrator</option>
+                                                    <option value="registered">Registered</option>
+                                                    <option value="edtior">Editor</option>
+                                                    <option value="guest">Guest</option>
+                                                </select>
                                             </div>
                                             <!-- Save changes button-->
-                                            <button class="btn btn-primary" type="button">Save changes</button>
+                                            <button class="btn btn-primary disabled" type="submit" name="updatePengguna">Save changes</button>
                                         </form>
                                     </div>
                                 </div>
