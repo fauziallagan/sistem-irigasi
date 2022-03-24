b6f4fc3bcf9432122a80e6a7a2c404010f6c59ae
fauziallagan <fauziallagan@gmail.com>
2022-03-24 15:15:53 +0700

    add datetime

--- api/api.php
+++ api/api.php
@@ -1,23 +1,29 @@
 <?php 
 require_once "../connection.php";
-// use file json in php
-$data = file_get_contents('input.json');
-header('content-type application/json');
-$raw = json_decode($data, true);
-// echo $data;
+
+// ambil data input dari file json
+// $raw = json_decode(file_get_contents('php://input'),true);
+
+$raw = json_decode(file_get_contents('input.json'), true); //Testing
+
+// testing json file
 // print_r($raw);
-$accel_x =  $raw['sensor']['0']['accel']['0']['x'];
-$accel_y = $raw['sensor']['0']['accel']['0']['y'];
-$accel_z = $raw['sensor']['0']['accel']['0']['z'];
-$compass_x = $raw['sensor']['0']['compass']['0']['x'];
-$compass_y = $raw['sensor']['0']['compass']['0']['y'];
-$compass_z = $raw['sensor']['0']['compass']['0']['z'];
-$gyro_x = $raw['sensor']['0']['gyro']['0']['x'];
-$gyro_y = $raw['sensor']['0']['gyro']['0']['y'];
-$gyro_z = $raw['sensor']['0']['gyro']['0']['z'];
-$gyro_temp = $raw['sensor']['0']['gyro']['0']['temp'];
-$dht_temp  = $raw['sensor']['0']['dht22']['0']['temp'];
-$dht_hum  = $raw['sensor']['0']['dht22']['0']['hum'];
+
+// buat objek json
+$json_return = new \stdClass();
+
+$accel_x      =  $raw['sensor']['0']['accel']['0']['x'];
+$accel_y      = $raw['sensor']['0']['accel']['0']['y'];
+$accel_z      = $raw['sensor']['0']['accel']['0']['z'];
+$compass_x    = $raw['sensor']['0']['compass']['0']['x'];
+$compass_y    = $raw['sensor']['0']['compass']['0']['y'];
+$compass_z    = $raw['sensor']['0']['compass']['0']['z'];
+$gyro_x       = $raw['sensor']['0']['gyro']['0']['x'];
+$gyro_y       = $raw['sensor']['0']['gyro']['0']['y'];
+$gyro_z       = $raw['sensor']['0']['gyro']['0']['z'];
+$gyro_temp    = $raw['sensor']['0']['gyro']['0']['temp'];
+$dht_temp     = $raw['sensor']['0']['dht22']['0']['temp'];
+$dht_hum      = $raw['sensor']['0']['dht22']['0']['hum'];
 // Analog 24
 $analog_24v1 = $raw['analog']['0']['24v']['0']['1'];
 $analog_24v2 = $raw['analog']['0']['24v']['0']['2'];
@@ -45,18 +51,28 @@ $digital_24v1 = $raw['digital']['0']['24v']['0']['1'];
 $digital_24v2 = $raw['digital']['0']['24v']['0']['2'];
 
 try{
-    $sql = "INSERT INTO testing(accel_x, accel_y, accel_z, compass_x,compass_y, compass_z, gyro_x,gyro_y,gyro_z,gyro_temp,dht_temp,dht_hum, analog_24v1,
+    $sql = "INSERT INTO testing(tanggal, waktu, accel_x, accel_y, accel_z, compass_x,compass_y, compass_z, gyro_x,gyro_y,gyro_z,gyro_temp,dht_temp,dht_hum, analog_24v1,
                                 analog_24v2,analog_24v3,analog_24v4,analog_12v1,analog_12v2,analog_12v3,analog_12v4, analog_5v1,analog_5v2,analog_5v3,
                                 analog_5v4, digital_5v1, digital_5v2, digital_12v1, digital_12v2, digital_24v1, digital_24v2)
-            VALUES($accel_x,$accel_y,$accel_z,$compass_x, $compass_y, $compass_z, $gyro_x,$gyro_y,$gyro_z,$gyro_temp, $dht_temp,$dht_hum, $analog_24v1,
+            VALUES(CURDATE(), CURTIME(), $accel_x,$accel_y,$accel_z,$compass_x, $compass_y, $compass_z, $gyro_x,$gyro_y,$gyro_z,$gyro_temp, $dht_temp,$dht_hum, $analog_24v1,
             $analog_24v2,$analog_24v3,$analog_24v4,$analog_12v1,$analog_12v2,$analog_12v3,$analog_12v4, $analog_5v1,$analog_5v2,$analog_5v3,$analog_5v4,
             $digital_5v1, $digital_5v2, $digital_12v1, $digital_12v2, $digital_24v1, $digital_24v2)";
 
     $data = $connection->query($sql);
     $data->setFetchMode(PDO::FETCH_ASSOC);
+    if($data){
+        $s_msg = "Sending Data Success!";
+    }else{
+        $s_msg = "Sending Data Error! Please Try Again!";
+    }
   }catch(PDOException $e ){
     die("Connection Failed!. Try Again!". $e->getMessage());
+
   }
+
+  $json_return->message = $s_msg;
+  header('Content-Type: application/json');
+  echo json_encode($json_return);
 
 
 ?>
--- api/getApi.php
+++ api/getApi.php
@@ -35,7 +35,6 @@ while($input = $row_input->fetch()){
     $analog_5v2 = $input['analog_5v2'];
     $analog_5v3 = $input['analog_5v3'];
     $analog_5v4 = $input['analog_5v4'];
-
     // Digital 5
     $digital_5v1 = $input['digital_5v1'];
     $digital_5v2 = $input['digital_5v2'];
@@ -45,55 +44,52 @@ while($input = $row_input->fetch()){
     // digital 24
     $digital_24v1 = $input['digital_24v1'];
     $digital_24v2 = $input['digital_24v2'];
+    
 
-    // echo $data["nama_pegawai"] . " ";
-    // echo $data["nik"] . " ";
-    // echo $data["alamat"];
 
     $json[] = array(
-    'accel_x'    => $input['accel_x'],
-    'accel_y'    => $input['accel_y'],
-    'accel_z'    => $input['accel_z'],
-    'compass_x'  => $input['compass_x'],
-    'compass_y'  => $input['compass_y'],
-    'compass_z'  => $input['compass_z'],
-    'gyro_x'     => $input['gyro_x'],
-    'gyro_y'     => $input['gyro_y'],
-    'gyro_z'     => $input['gyro_z'],
-    'gyro_temp'  => $input['gyro_temp'],
-    'dht_temp'   => $input['dht_temp'],
-    'dht_hum'    => $input['dht_hum'],
-    'analog_24v1' => $input['analog_24v1'],
-    'analog_24v2' => $input['analog_24v2'],
-    'analog_24v3' => $input['analog_24v3'],
-    'analog_24v4' => $input['analog_24v4'],
-    // Analog 12
-    'analog_12v1' => $input['analog_12v1'],
-    'analog_12v2' => $input['analog_12v2'],
-    'analog_12v3' => $input['analog_12v3'],
-    'analog_12v4' => $input['analog_12v4'],
-    // Analog 5
-    'analog_5v1' => $input['analog_5v1'],
-    'analog_5v2' => $input['analog_5v2'],
-    'analog_5v3' => $input['analog_5v3'],
-    'analog_5v4' => $input['analog_5v4'],
-    // Digital 5
-    'digital_5v1' => $input['digital_5v1'],
-    'digital_5v2' => $input['digital_5v2'],
-    // digital 12
-    'digital_12v1' => $input['digital_12v1'],
-    'digital_12v2' => $input['digital_12v2'],
-    // digital 24
-    'digital_24v1' => $input['digital_24v1'],
-    'digital_24v2' => $input['digital_24v2'],
+        'accel_x'    => $input['accel_x'],
+        'accel_y'    => $input['accel_y'],
+        'accel_z'    => $input['accel_z'],
+        'compass_x'  => $input['compass_x'],
+        'compass_y'  => $input['compass_y'],
+        'compass_z'  => $input['compass_z'],
+        'gyro_x'     => $input['gyro_x'],
+        'gyro_y'     => $input['gyro_y'],
+        'gyro_z'     => $input['gyro_z'],
+        'gyro_temp'  => $input['gyro_temp'],
+        'dht_temp'   => $input['dht_temp'],
+        'dht_hum'    => $input['dht_hum'],
+        'analog_24v1' => $input['analog_24v1'],
+        'analog_24v2' => $input['analog_24v2'],
+        'analog_24v3' => $input['analog_24v3'],
+        'analog_24v4' => $input['analog_24v4'],
+        // Analog 12
+        'analog_12v1' => $input['analog_12v1'],
+        'analog_12v2' => $input['analog_12v2'],
+        'analog_12v3' => $input['analog_12v3'],
+        'analog_12v4' => $input['analog_12v4'],
+        // Analog 5
+        'analog_5v1' => $input['analog_5v1'],
+        'analog_5v2' => $input['analog_5v2'],
+        'analog_5v3' => $input['analog_5v3'],
+        'analog_5v4' => $input['analog_5v4'],
+        // Digital 5
+        'digital_5v1' => $input['digital_5v1'],
+        'digital_5v2' => $input['digital_5v2'],
+        // digital 12
+        'digital_12v1' => $input['digital_12v1'],
+        'digital_12v2' => $input['digital_12v2'],
+        // digital 24
+        'digital_24v1' => $input['digital_24v1'],
+        'digital_24v2' => $input['digital_24v2'],
     );
 }
-    $response = array(
-        'status'=>'OK',
-        'data Sensor'=>$json
-    );
-    echo json_encode($response);
-    header('content-type application/json');
-    print_r($response);
-
+$response = array(
+    'status'=>'OK',
+    'data Sensor'=>$json,
+    'message'=>'Success!'
+);
+header('Content-Type:application/json');
+echo json_encode($response);
 ?>
